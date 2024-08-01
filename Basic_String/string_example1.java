d221e2e9a26a31c41add23075bc7130719a86a2f
Tiwari2405 <tiwarianku2001@gmail.com>
2024-08-01 11:43:49 +0530

    01/08/2024

--- Basic_String/string_example1.java
+++ Basic_String/string_example1.java
@@ -5,5 +5,7 @@ public class string_example1 {
     public static void main(String args[]) {
         String s = "Ankit";
         System.out.println(s);
+
+        String s2 = "";
     }
 }
---
+++ Basic_String/string_reverseString.java
@@ -0,0 +1,11 @@
+package Basic_String;
+
+public class string_reverseString {
+    public static void main(String[] args) {
+        String s1 = "Tiwari";
+
+        for (int i = s1.length()-1; i >=0; i--) {
+            System.out.print(s1.charAt(i));
+        }
+    }
+}
